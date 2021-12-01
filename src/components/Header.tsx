5ff0e1f33295f3a4390ab3c657f0d5ee75a44ef9
Jian Ong <jian.ong@rea-group.com>
2021-12-01 17:25:03 +1100

    Center refresh button content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ import { RiRefreshLine } from "react-icons/ri";
 
 const ParentHeader = styled.div`
   width: 100vw;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `;
-interface Props {
-  clickHandler: (event: React.MouseEvent<HTMLButtonElement>) => void;
-}
 
 const RefreshBtn = styled.button`
   height: 50px;
@@ -18,13 +18,24 @@ const RefreshBtn = styled.button`
   border-radius: 25px;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  font-size: medium;
+  font-size: 1em;
   cursor: pointer;
   &:active {
     box-shadow: 7px 6px 28px 1px rgba(0, 0, 0, 0.24);
     transform: translateY(2px);
   }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > *:first-child {
+    margin-right: 4px;
+  }
 `;
+
+interface Props {
+  clickHandler: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
 
 const Header = ({ clickHandler }: Props) => (
   <ParentHeader>
